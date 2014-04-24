4e02482a5c7a150f69a7f00c225839db6f8fcf10
Brian Bird <birdb@lanecc.edu>
2014-04-23 17:50:31 -0700

    Initial commit
    Demo of one-to-many and many-to-many relationships using Entity
    Framework with code-first.

---
+++ ManyToMany/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyToMany
+{
+    class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public List<Club> Clubs { get; set; }
+    }
+
+    class Club
+    {
+        public int ClubId { get; set; }
+        public string Name { get; set; }
+        public List<Person> People { get; set; }
+    }
+
+    class ManyToManyDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AddPeopleAndClubs();
+            ShowClubs("Brian");
+        }
+
+        static void AddPeopleAndClubs()
+        {
+            ManyToManyDbContext context = new ManyToManyDbContext();
+
+            Person brian = new Person() { Name = "Brian" };
+            brian.Clubs = new List<Club>();
+
+            Club eadnug = new Club() { Name = "Eugene Area DotNet User's Group" };
+            brian.Clubs.Add(eadnug);
+            Club wna = new Club() { Name = "Washburne Neighborhood Association" };
+            brian.Clubs.Add(wna);
+
+            Person amanda = new Person() { Name = "Amanda" };
+            amanda.Clubs = new List<Club>();
+            amanda.Clubs.Add(wna);
+
+            Club nanowrimo = new Club() { Name = "National Novel Writing Month" };
+            amanda.Clubs.Add(nanowrimo);
+
+            context.People.Add(brian);
+            context.People.Add(amanda);
+            context.SaveChanges();
+        }
+
+        static void ShowClubs(string personName)
+        {
+            ManyToManyDbContext context = new ManyToManyDbContext();
+            Person person = (from p in context.People
+                             where p.Name == personName
+                             select p).Include(p => p.Clubs).FirstOrDefault();
+            foreach (Club club in person.Clubs)
+                Console.WriteLine(club.Name);
+        }
+    }
+}
---
+++ OneToMany/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneToMany
+{
+    class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+    }
+
+    class County
+    {
+        public int CountyId { get; set; }
+        public string CountyName { get; set; }
+        public List<City> Cities { get; set; }
+    }
+
+    class OneToManyDbContext : DbContext
+    {
+        public DbSet<County> Counties { get; set; }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AddCountyAndCities();
+            ShowCities("Lane");
+        }
+
+        static void AddCountyAndCities()
+        {
+            OneToManyDbContext context = new OneToManyDbContext();
+
+            County lane = new County() { CountyName = "Lane" };
+
+            lane.Cities= new List<City>();
+            lane.Cities.Add(new City() { CityName = "Springfield" });
+            lane.Cities.Add(new City() { CityName = "Eugene" });
+
+            context.Counties.Add(lane);
+            context.SaveChanges();
+        }
+
+        static void ShowCities(string countyName)
+        {
+            OneToManyDbContext context = new OneToManyDbContext();
+            County county = (from c in context.Counties 
+                             where c.CountyName == countyName 
+                             select c).Include(c => c.Cities).FirstOrDefault();
+            foreach (City city in county.Cities)
+                Console.WriteLine(city.CityName);
+        }
+    }
+}
